31a1152f09952d3fb72a36bee102c512aa2ceb4f
Matheus Alexandre <alexandrematheusvieira5@gmail.com>
2026-06-06 10:02:21 -0300

    Atualização do projeto

--- app.py
+++ app.py
@@ -14,6 +14,14 @@ def inicial():
 def login():
     return render_template('login.html')
 
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 @app.route('/resultado', methods=['POST'])
 def resultado():
 
@@ -48,6 +56,7 @@ def resultado():
         pontos += 3
 
     # Resultado
+       # Resultado
     if pontos <= 3:
         resultado = "Não recomendado"
     elif pontos <= 6:
@@ -55,11 +64,19 @@ def resultado():
     else:
         resultado = "Viável"
 
+    economia = round(consumo * 0.85, 2)
+
+    irradiacao_itamaraca = 5.6
+
     return render_template(
         'resultado.html',
         resultado=resultado,
-        pontos=pontos
+        pontos=pontos,
+        economia=economia,
+        irradiacao=irradiacao_itamaraca
     )
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
